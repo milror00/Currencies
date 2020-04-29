62437c6df633cdafff2f3b041fce4c102cfaef6e
oem <Oem@OEM>
2020-04-29 13:31:21 +0100

    added pre-commit and black

--- app/yahoo_currencies_scraper.py
+++ app/yahoo_currencies_scraper.py
@@ -17,25 +17,24 @@ Modify:
 """
 
 
-class YahooCurrencies():
-
+class YahooCurrencies:
     def __init__(self):
-        self.url = 'https://finance.yahoo.com/currencies'
+        self.url = "https://finance.yahoo.com/currencies"
 
     def getURL(self):
         try:
             request = requests.get(url=self.url)
-            request.encoding = 'utf-8'
+            request.encoding = "utf-8"
             html = request.text
-            self.page_bf = BeautifulSoup(html, 'lxml')
+            self.page_bf = BeautifulSoup(html, "lxml")
         except Exception as e:
             print(e)
             exit(1)
 
     def getAllTableRows(self):
         # all table rows
-        tables = self.page_bf.findChildren('table')
-        rows = tables[0].findChildren(['th', 'tr'])
+        tables = self.page_bf.findChildren("table")
+        rows = tables[0].findChildren(["th", "tr"])
         return rows
 
     def getAllCurrencies(self):
@@ -43,37 +42,40 @@ class YahooCurrencies():
         rows = self.getAllTableRows()
         results = []
         for row in rows:
-            cells = row.findChildren('td')
+            cells = row.findChildren("td")
             currency = {}
             for cell in cells:
-                currency['symbol'] = cells[0].string
-                currency['name'] = cells[1].string
-                currency['lastPrice'] = cells[2].string
-                currency['change'] = cells[3].string
-                currency['%change'] = cells[4].string
+                currency["symbol"] = cells[0].string
+                currency["name"] = cells[1].string
+                currency["lastPrice"] = cells[2].string
+                currency["change"] = cells[3].string
+                currency["%change"] = cells[4].string
                 results.append(currency)
                 break
         return results
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     currencies = YahooCurrencies()
     results = currencies.getAllCurrencies()
     # headers
-    print('|{0: <10}|{1: <20}|{2: <15}|{3: <15}|{4: <15}|'.format(
-        'Symbol',
-        'Name',
-        'Last Price',
-        'Change',
-        '%change'))
-    print('|----------|-------------------|---------------|---------------' +
-          '|---------------|')
+    print(
+        "|{0: <10}|{1: <20}|{2: <15}|{3: <15}|{4: <15}|".format(
+            "Symbol", "Name", "Last Price", "Change", "%change"
+        )
+    )
+    print(
+        "|----------|-------------------|---------------|---------------"
+        + "|---------------|"
+    )
     # data
     for currency in results:
-        print('|{0: <10}|{1: <20}|{2: <15}|{3: <15}|{4: <15}|'.format(
-            currency['symbol'],
-            currency['name'],
-            currency['lastPrice'],
-            currency['change'],
-            currency['%change'],
-        ))
+        print(
+            "|{0: <10}|{1: <20}|{2: <15}|{3: <15}|{4: <15}|".format(
+                currency["symbol"],
+                currency["name"],
+                currency["lastPrice"],
+                currency["change"],
+                currency["%change"],
+            )
+        )
